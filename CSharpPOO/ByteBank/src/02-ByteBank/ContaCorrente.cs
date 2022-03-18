b13f9d3fa40270634576a6676d38795e93f78f39
Francisco <a124fr@gmail.com>
2022-03-18 13:03:19 -0300

    Criando projeto, e trabalhando com valor padrão ao instancia objeto

---
+++ CSharpPOO/ByteBank/src/02-ByteBank/ContaCorrente.cs
@@ -0,0 +1,12 @@
+﻿
+namespace _02_ByteBank
+{
+
+    class ContaCorrente
+    {
+        public string titular;
+        public int agencia;
+        public int numero;
+        public double saldo = 100.0;
+    }
+}
