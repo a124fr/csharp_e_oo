0f39bd1ca3d72297fa761d72db4813971a6d76b8
Francisco <a124fr@gmail.com>
2022-03-19 12:16:56 -0300

    Trabalhando com composição, namespace e valores null

---
+++ CSharpPOO/ByteBank/src/05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,42 @@
+﻿
+namespace _05_ByteBank
+{
+
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double saldo = 100.0;
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor || this.saldo <= 0)
+            {
+                return false;
+            }
+
+            this.saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                this.saldo += valor;
+            }
+        }
+
+        public bool Trasnferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.Sacar(valor))
+            {
+                contaDestino.Depositar(valor);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ CSharpPOO/ByteBank/src/05-ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente gabriela = new Cliente();
+            gabriela.cpf = "434.562.878-20";
+            gabriela.nome = "Gabriela";
+            gabriela.profissao = "Desenvolvedora C#";
+
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = gabriela;
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            conta.titular.nome = "Gabriela Costa";
+
+            Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
