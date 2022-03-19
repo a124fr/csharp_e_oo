672854cf49814b79606a5a25505263512aaaf4d4
Francisco <a124fr@gmail.com>
2022-03-19 12:56:54 -0300

    Trabalhando o encapsulamento dos atributos

---
+++ CSharpPOO/ByteBank/src/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,57 @@
+﻿
+namespace _06_ByteBank
+{
+
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        private double saldo = 100.0;
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor || saldo <= 0)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                saldo += valor;
+            }
+        }
+
+        public bool Trasnferir(double valor, ContaCorrente contaDestino)
+        {
+            if (Sacar(valor))
+            {
+                contaDestino.Depositar(valor);
+                return true;
+            }
+
+            return false;
+        }
+
+        public double GetSaldo()
+        {
+            return saldo;
+        }
+
+        public void SetSaldo(double saldo)
+        {
+            if (saldo < 0)
+            {
+                return;
+            }
+
+            this.saldo = saldo;
+        }
+    }
+}
