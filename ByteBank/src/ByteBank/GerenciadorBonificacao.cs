2360f87ebb42c7d46dab9a46e1aa9e9efb92cb97
Francisco <a124fr@gmail.com>
2022-03-21 18:39:45 -0300

    Trabalhando com herança e sobrescrita de método com virtual e override

---
+++ ByteBank/src/ByteBank/GerenciadorBonificacao.cs
@@ -0,0 +1,24 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class GerenciadorBonificacao
+    {
+        private double _totalBonficacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonficacao += funcionario.GetBonificacao();
+        }
+
+        public double GetTotalBonficacao()
+        {
+            return _totalBonficacao;
+        }
+    }
+}
--- ByteBank/src/ByteBank/Program.cs
+++ ByteBank/src/ByteBank/Program.cs
@@ -11,13 +11,29 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Funcionario carlos = new Funcionario();
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
 
+            Funcionario carlos = new Funcionario();
             carlos.Nome = "Carlos";
             carlos.CPF = "546.879.157-20";
             carlos.Salario = 2000;
 
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "454.658.148-3";
+            roberta.Salario = 5000;
+                        
+            gerenciador.Registrar(carlos);
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine(carlos.Nome);
             Console.WriteLine(carlos.GetBonificacao());
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total Bonificacão: " + gerenciador.GetTotalBonficacao());
+
             Console.ReadLine();
         }
     }
